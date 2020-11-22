98da47237cd4e9b05f09074a671a0fcfca1bba30
Andrei Serban <andreiserbanrazvan@gmail.com>
2020-11-22 19:25:28 +0200

    doom p1

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 1.5f;
+    public float smoothing = 1.5f;
+
+
+    private float xMousePos;
+    private float smoothMousePos;
+
+    private float currentLookingPos;
+   
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        GetInput();
+        ModifyInput();
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        currentLookingPos += smoothMousePos;
+        transform.localRotation = Quaternion.AngleAxis(currentLookingPos,transform.up);
+    }
+
+    private void ModifyInput()
+    {
+        xMousePos *= sensitivity * smoothing;
+        smoothMousePos = Mathf.Lerp(smoothMousePos, xMousePos, 1f / smoothing);
+    }
+
+    private void GetInput()
+    {
+        xMousePos = Input.GetAxisRaw("Mouse X");
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 20f;
+    private CharacterController characterController;
+    public Animator camAnim;
+    private bool isWalking;
+
+    private Vector3 inputVector;
+    private Vector3 movementVector;
+
+    private float gravity = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        MovePlayer();
+        CheckForHeadBob();
+
+        camAnim.SetBool("isWalking", isWalking);
+
+    }
+
+    void MovePlayer()
+    {
+        characterController.Move(movementVector * Time.deltaTime);
+    }
+
+    void GetInput()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        inputVector.Normalize();
+        inputVector = transform.TransformDirection(inputVector);
+
+        movementVector = (inputVector * speed) + (Vector3.up * gravity);
+    
+    }
+
+    void CheckForHeadBob()
+    {
+        if(characterController.velocity.magnitude>0.1f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+}
